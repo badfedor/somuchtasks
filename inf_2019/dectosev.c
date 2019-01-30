0f2d826f673cc1006fc8b222a44e14df4a2e66c5
Fedor <apache32@yandex.ru>
2019-01-30 23:15:44 +0300

    upd dectosev.c

--- inf_2019/dectosev.c
+++ inf_2019/dectosev.c
@@ -1,25 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void decto(int i, const int t){
+	int c=0, di=i, dc=c;
+	if(i==0){
+		printf("0\n");
+		return;
+	}
+	while(di>0){
+		di=di/t; dc++;
+		
+	}
+	int a[dc];
+	while(i!=0){
+		a[c]=i%t;
+		i=i/t;
+		c++;
+	}	
+	int j;
+	for(j=c; j>0; j--){
+		printf("%d", a[j-1]);
+		a[j-1] = 0;
+	}
+	printf("\n");
+}
+
 int main(){
-	int t;
-	printf("Radix? "); scanf("%d", &t);
-	for(int i=0; i<10; i++){
-		int c=0, di=i, dc=c;
-		while(di!=0){
-			di=di/t; dc++;
-		}
-		int a[dc];
-		while(i!=0){
-			a[c]=i%t;
-			i=i/t;
-			c++;
-		}	
-		for(int j=c; j>0; j--){
-			printf("%d", a[j-1]);
-			a[j-1] = 0;
-		}
-		printf("\n");
+	int radix, a;
+	unsigned int num;
+	printf("Radix? "); scanf("%d", &radix);
+	if(radix < 2 || radix > 10){ return 0; }
+	printf("From 0 to "); scanf("%d", &num);
+	for(a=0; a<=num; a++){
+		printf("%d - ", a);
+		decto(a, radix);
 	}
 	return 0;
 }
