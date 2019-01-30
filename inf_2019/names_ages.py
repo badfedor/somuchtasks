3d4d1c7d985fd384bdbdbb7efc94a0f92fd32e4f
Fedor <apache32@yandex.ru>
2019-01-30 22:50:33 +0300

    upd names_ages.py

--- inf_2019/names_ages.py
+++ inf_2019/names_ages.py
@@ -1,6 +1,8 @@
-names = ['Антон', 'Борис', 'Виктор']
+names = ['Федос', 'Мыш', 'Ижанитов', 'Влад', 'Игорь', 'Ванесса', ]   # use this array for names 
 age = []
-for x in names:
+
+for x in range(0, len(names)):
+    print(names[x])
     age.append(int(input()))
 
 for x in range(len(age)-1,0,-1):
@@ -12,21 +14,34 @@ for x in range(len(age)-1,0,-1):
             names[i] = names[i+1]
             age[i+1] = temp_age
             names[i+1] = temp_name
-
-uniq_names = [0]
-
-for x in range(0, len(age)):
-	if age[x] not in age:
-		
-	else:
-		uniq_names.append(names[x])
+    
+uniq_state = False
 
 for x in range(0, len(age)):
-    print(names[x], ' ', age[x])
+    if x<len(age)-1 and age[x] == age[x+1]:
+        uniq_state = True
 
-if len(uniq_names) == 1:
-	print(names[-1], 'старше всех')
+if uniq_state == True:
+    final_names = ''
 
-elif len(uniq_names) > 1:
-	print(uniq_names, 'старше')
+    names.reverse()
+    age.reverse()
+
+    if(len(set(age))==1):
+        for x in range(0, len(age)):
+            final_names += str(names[x] + ', ')
+        final_names = final_names[:-2]
+        final_names += ' одинаковы по возрасту.'
+    else:
+        for x in range(0, len(age)):
+            if x<len(age)-1:
+                if age[x] > age[x+1]:
+                    final_names += str(names[x] + ' старше чем ' + names[x+1] + ', ')
+                if age[x] == age[x+1]:
+                    final_names += str(names[x+1] + ', ')
+        final_names = final_names[:-2]
+
+    print(final_names)
+elif uniq_state == False:
+    print(names[-1], 'старше всех')
 
